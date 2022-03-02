9bd1f36f2381e03b87ce55bcb680ec8061722468
MMattia-dev <kromolski@gmail.com>
2022-03-02 17:50:44 +0100

    Dodaj pliki projektów.

---
+++ ME_LE_ChangeLanguage/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿
+namespace ME_LE_ChangeLanguage
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.button3 = new System.Windows.Forms.Button();
+            this.metroProgressSpinner1 = new MetroFramework.Controls.MetroProgressSpinner();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            resources.ApplyResources(this.button1, "button1");
+            this.button1.Name = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton1);
+            resources.ApplyResources(this.groupBox1, "groupBox1");
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.TabStop = false;
+            // 
+            // radioButton1
+            // 
+            resources.ApplyResources(this.radioButton1, "radioButton1");
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.TabStop = true;
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // textBox1
+            // 
+            resources.ApplyResources(this.textBox1, "textBox1");
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            // 
+            // button2
+            // 
+            resources.ApplyResources(this.button2, "button2");
+            this.button2.Name = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            resources.ApplyResources(this.button3, "button3");
+            this.button3.Name = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // metroProgressSpinner1
+            // 
+            this.metroProgressSpinner1.BackColor = System.Drawing.SystemColors.Control;
+            resources.ApplyResources(this.metroProgressSpinner1, "metroProgressSpinner1");
+            this.metroProgressSpinner1.Maximum = 100;
+            this.metroProgressSpinner1.Minimum = 20;
+            this.metroProgressSpinner1.Name = "metroProgressSpinner1";
+            this.metroProgressSpinner1.UseCustomBackColor = true;
+            this.metroProgressSpinner1.UseSelectable = true;
+            this.metroProgressSpinner1.Value = 70;
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.metroProgressSpinner1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Button button3;
+        private MetroFramework.Controls.MetroProgressSpinner metroProgressSpinner1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ ME_LE_ChangeLanguage/Form1.cs
@@ -0,0 +1,241 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+
+namespace ME_LE_ChangeLanguage
+{
+    public partial class Form1 : Form
+    {
+        BackgroundWorker worker = new BackgroundWorker();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            worker.WorkerSupportsCancellation = true;
+            worker.WorkerReportsProgress = true;
+
+            worker.DoWork += Worker_DoWork;
+        }
+
+        private void Worker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            string message = "Wymagane jest stworzenie kopii zapasowej plików językowych." + "\nCzy chcesz to zrobić?\n\n" +
+                "Upewnij się, że masz wystarczającą ilość miejsca na dysku.\nME1: 21,7 GB"; //\nME2: 7,95 GB
+            string title = "Uwaga!";
+            MessageBoxButtons buttons = MessageBoxButtons.YesNo;
+            DialogResult result = MessageBox.Show(message, title, buttons, MessageBoxIcon.Warning);
+            if (result == DialogResult.Yes)
+            {
+                button1.BeginInvoke(new Action(() => { button1.Enabled = false; }));
+                button2.BeginInvoke(new Action(() => { button2.Enabled = false; }));
+                button3.BeginInvoke(new Action(() => { button3.Enabled = false; }));
+                groupBox1.BeginInvoke(new Action(() => { groupBox1.Enabled = false; }));
+                metroProgressSpinner1.BeginInvoke(new Action(() => { metroProgressSpinner1.Visible = true; }));
+                label1.BeginInvoke(new Action(() => { label1.Visible = true; }));
+
+                string lang_folder_me1 = textBox1.Text + @"\Game\ME1\BioGame\Content\Packages\ISACT";
+                string lang_folder_me1_pcc = textBox1.Text + @"\Game\ME1\BioGame\CookedPCConsole";
+                string lang_folder_me2 = "";
+
+                if (radioButton1.Checked == true)
+                {
+                    //tutaj stwórz folder z kopiami
+                    Directory.CreateDirectory(lang_folder_me1 + @"\kopia_lang");
+                    Directory.CreateDirectory(lang_folder_me1_pcc + @"\kopia_lang");
+
+
+                    //tutaj stwórz kopie
+                    string sourcePath = lang_folder_me1;
+                    string sourcePath_pcc = lang_folder_me1_pcc;
+                    string targetPath = lang_folder_me1 + @"\kopia_lang";
+                    string targetPath_pcc = lang_folder_me1_pcc + @"\kopia_lang";
+
+                    foreach (var sourceFilePath in Directory.GetFiles(sourcePath))
+                    {
+                        string fileName = Path.GetFileName(sourceFilePath);
+                        string destinationFilePath = Path.Combine(targetPath, fileName);
+
+                        File.Copy(sourceFilePath, destinationFilePath, true);
+                    }
+                    foreach (var sourceFilePath_pcc in Directory.GetFiles(sourcePath_pcc))
+                    {
+                        string fileName = Path.GetFileName(sourceFilePath_pcc);
+                        string destinationFilePath = Path.Combine(targetPath_pcc, fileName);
+
+                        File.Copy(sourceFilePath_pcc, destinationFilePath, true);
+                    }
+
+
+
+                    //tutaj zmien jezyk
+                    var extenstions = new List<string> { "isb" };
+                    var extenstions_pcc = new List<string> { "pcc" };
+                    var files = Directory.EnumerateFiles(lang_folder_me1, "*.*", SearchOption.TopDirectoryOnly).Where(x => extenstions.Contains(Path.GetExtension(x).TrimStart('.').ToLowerInvariant()));
+                    var files_pcc = Directory.EnumerateFiles(lang_folder_me1_pcc, "*.*", SearchOption.TopDirectoryOnly).Where(x => extenstions_pcc.Contains(Path.GetExtension(x).TrimStart('.').ToLowerInvariant()));
+
+                    foreach (var file in files)
+                    {
+                        string file2 = Path.GetFileNameWithoutExtension(file);
+
+                        if (file.Contains("_plpc"))
+                        {
+                            //string file2_substr = file2.Substring(0, file2.Length - 5);
+                            //string filter1_file = lang_folder_me1 + @"\" + file2_substr + ".isb";
+
+                            //if (File.Exists(filter1_file))
+                            //{
+                            //    string f = Path.GetFileNameWithoutExtension(filter1_file);
+
+                            //    File.Move(filter1_file, filter1_file.Replace(f, f + "+"));
+                            //    File.Move(file, file.Replace("_plpc", ""));
+
+                            //    string filter2_file = lang_folder_me1 + @"\" + file2_substr + "+" + ".isb";
+                            //    File.Move(filter2_file, filter2_file.Replace("+", "_plpc"));
+                            //}
+
+                            File.Move(file, file.Replace("_plpc", "_plkopia"));
+                        }
+                    }
+
+                    foreach (var file in files_pcc)
+                    {
+                        string file2 = Path.GetFileNameWithoutExtension(file);
+
+                        if (file.Contains("_PLPC"))
+                        {
+                            string file2_substr = file2.Substring(0, file2.Length - 5);
+                            string filter1_file = lang_folder_me1_pcc + @"\" + file2_substr + ".pcc";
+
+                            if (File.Exists(filter1_file))
+                            {
+                                string f = Path.GetFileNameWithoutExtension(filter1_file);
+
+                                File.Move(filter1_file, filter1_file.Replace(f, f + "+"));
+                                File.Move(file, file.Replace("_PLPC", ""));
+
+                                string filter2_file = lang_folder_me1_pcc + @"\" + file2_substr + "+" + ".pcc";
+                                File.Move(filter2_file, filter2_file.Replace("+", "_PLPC"));
+                            }
+                        }
+
+                        if (file.Contains("_LOC_PLPC"))
+                        {
+                            string file2_substr = file2.Substring(0, file2.Length - 5);
+                            string filter1_file = lang_folder_me1_pcc + @"\" + file2_substr + "_INT" + ".pcc";
+
+                            if (File.Exists(filter1_file))
+                            {
+                                string f = Path.GetFileNameWithoutExtension(filter1_file);
+
+                                File.Move(filter1_file, filter1_file.Replace(f, f + "+"));
+                                File.Move(file, file.Replace("_PLPC", "_INT"));
+
+                                string filter2_file = lang_folder_me1_pcc + @"\" + file2_substr + "_INT+" + ".pcc";
+                                File.Move(filter2_file, filter2_file.Replace("_INT+", "_PLPC"));
+                            }
+                        }
+                    }
+
+                    MessageBox.Show("Kopia zapasowa została stworzona. Język zmieniony.");
+
+                    button2.BeginInvoke(new Action(() => { button2.Enabled = true; }));
+                    button3.BeginInvoke(new Action(() => { button3.Enabled = true; }));
+                    groupBox1.BeginInvoke(new Action(() => { groupBox1.Enabled = true; }));
+                    metroProgressSpinner1.BeginInvoke(new Action(() => { metroProgressSpinner1.Visible = false; }));
+                    label1.BeginInvoke(new Action(() => { label1.Visible = false; }));
+                }
+
+                //if (radioButton2.Checked == true)
+                //{
+
+                //}
+
+                button1.BeginInvoke(new Action(() => { button1.Enabled = true; }));
+            }
+
+            if (result == DialogResult.No)
+            {
+
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Ustaw ścieżkę do folderu\n'Mass Effect Legendary Edition', nie do jego podfolderów!");
+            DialogResult result = folderBrowserDialog1.ShowDialog();
+
+            if (folderBrowserDialog1.SelectedPath.Contains("Mass Effect Legendary Edition"))
+            {
+                textBox1.Text = folderBrowserDialog1.SelectedPath;
+                groupBox1.Enabled = true;
+                textBox1.Enabled = true;
+            }
+            else
+            {
+                MessageBox.Show("Podana ścieżka nie zawiera nazwy gry. Spróbuj jeszcze raz.");
+            }
+
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked == true)
+            {
+                button3.Enabled = true;
+
+                if (Directory.Exists(textBox1.Text + @"\Game\ME1\BioGame\Content\Packages\ISACT\kopia_lang"))
+                {
+                    button1.Enabled = true;
+                }
+                else
+                {
+                    button1.Enabled = false;
+                }
+            }
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            //if (radioButton2.Checked == true)
+            //{
+            //    button3.Enabled = true;
+
+            //    //
+            //}
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            worker.RunWorkerAsync();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked == true)
+            {
+                Process.Start(textBox1.Text + @"\Game\ME1\BioGame\Content\Packages\ISACT\kopia_lang");
+                Process.Start(textBox1.Text + @"\Game\ME1\BioGame\CookedPCConsole\kopia_lang");
+            }
+
+            //if (radioButton2.Checked == true)
+            //{
+            //    //
+            //}
+        }
+
+    }
+}
